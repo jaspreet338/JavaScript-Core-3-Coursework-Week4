08c01e1ff918230112b46b8a0b91e8152f1cb451
jaspreet kaur <kaurjas1928@yahoo.com>
2022-11-10 19:33:37 +0000

    updated car-sales test

--- A-pass-tests/09-car-sales/car-sales.js
+++ A-pass-tests/09-car-sales/car-sales.js
@@ -1,3 +1,29 @@
-function sales(carsSold) {}
+function sales(carsSold) {
+   let fordTotal = 0;
+   let landRoverTotal = 0;
+   let toyotaTotal = 0;
+   let hondaTotal = 0;
+
+   for(let i = 0; i < carsSold.length; i++){
+    if(carsSold[i].make === 'Ford') {
+        fordTotal += carsSold[i].price;
+    }else if(carsSold[i].make === 'Land Rover'){
+        landRoverTotal += carsSold[i].price;
+    }else if(carsSold[i].make === 'Toyota') {
+        toyotaTotal += carsSold[i].price;
+    }else if(carsSold[i].make === 'Honda') {
+        hondaTotal += carsSold[i].price;
+    } else {
+        return 'not an array'
+    }
+   }
+   return {
+    Ford: fordTotal,
+    "Land Rover": landRoverTotal,
+    Toyota: toyotaTotal,
+    Honda: hondaTotal
+   }
+
+}
 
 module.exports = sales;
