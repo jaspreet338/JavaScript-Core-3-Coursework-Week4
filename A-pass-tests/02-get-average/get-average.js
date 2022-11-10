73d1cf2d60bfd1abda7e5d7486c9932f3bd9e5b7
jaspreet kaur <kaurjas1928@yahoo.com>
2022-11-10 19:13:58 +0000

    updated get-average test

--- A-pass-tests/02-get-average/get-average.js
+++ A-pass-tests/02-get-average/get-average.js
@@ -2,6 +2,15 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+    let filter = numbers.filter((e) => {
+        return typeof e === 'number' 
+    })
+    let sum = 0;
+    for(let i=0; i<filter.length; i++) {
+        sum += filter[i];
+    }
+    return sum / filter.length;
+}
 
 module.exports = average;
