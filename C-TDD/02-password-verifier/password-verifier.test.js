55fe3e9cef7f2392f14c994bc56893731d2fa341
jaspreet kaur <kaurjas1928@yahoo.com>
2022-11-11 12:38:44 +0000

    updated password-verifier

---
+++ C-TDD/02-password-verifier/password-verifier.test.js
@@ -0,0 +1,51 @@
+let verify = require("./password-verifier");
+
+describe("requirement 1", () => {
+    test("return Password rejected if the password is less than 8 characters", function () {
+        const password = "Jas";
+        const result = verify(password);
+        const actualOutput = "Password rejected";
+
+        expect(result).toEqual(actualOutput);
+    });
+} )
+
+describe("requirement 2", () => {
+  test("return Password rejected if the password is null", function () {
+    const password = "";
+    const result = verify(password);
+    const actualOutput = "Password rejected";
+
+    expect(result).toEqual(actualOutput);
+  });
+});
+
+describe("requirement 3", () => {
+  test("return Password rejected if the password does not have at least 1 uppercase letter ", function () {
+    const password = "jaspreet";
+    const result = verify(password);
+    const actualOutput = "Password rejected";
+
+    expect(result).toEqual(actualOutput);
+  });
+});
+
+describe("requirement 4", () => {
+  test("return Password rejected if the password does not have at least 1 number", function () {
+    const password = "Jaspreet";
+    const result = verify(password);
+    const actualOutput = "Password rejected";
+
+    expect(result).toEqual(actualOutput);
+  });
+});
+
+describe("requirement 5", () => {
+  test("return Password accepted if the password meet all criteria", function () {
+    const password = "Jaskaur1";
+    const result = verify(password);
+    const actualOutput = "Password rejected";
+
+    expect(result).toEqual(actualOutput);
+  });
+});
